88dd3da871e6d42efba2621d1d866f3ec68be710
alphascorp <85639455+alphascorp@users.noreply.github.com>
2021-06-09 23:54:54 +0200

    Update remount-sysvol.sh

--- payloads/remount-sysvol.sh
+++ payloads/remount-sysvol.sh
@@ -100,7 +100,7 @@ SVPL="$VOLUME"/System/Library/CoreServices/SystemVersion.plist
 SVPL_VER=`fgrep '<string>10' "$SVPL" | sed -e 's@^.*<string>10@10@' -e 's@</string>@@' | uniq -d`
 SVPL_BUILD=`grep '<string>[0-9][0-9][A-Z]' "$SVPL" | sed -e 's@^.*<string>@@' -e 's@</string>@@'`
 
-if echo $SVPL_BUILD | grep -q '^20'
+if echo $SVPL_BUILD | grep -q '^21'
 then
     ecrr -n "Volume appears to have a Monterey installation (build" $SVPL_BUILD
     ecrr "). Continuing."
